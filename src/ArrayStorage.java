206f14ea0c5fcc0ae8d50934126dab1501c08f23
GeisAnn <pushyduke@yandex.ru>
2021-10-01 11:11:02 +0300

    Update clear

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,7 +9,7 @@ public class ArrayStorage {
     int size;
 
     void clear() {
-        Arrays.fill(storage, 0, size - 1, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
