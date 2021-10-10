adbeb58178ee697bf291c55775cfbbb34eb70e60
GeisAnn <pushyduke@yandex.ru>
2021-10-10 21:14:42 +0300

    Introduce interface

---
+++ src/com/geisann/webapp/storage/Storage.java
@@ -0,0 +1,23 @@
+package com.geisann.webapp.storage;
+
+import com.geisann.webapp.model.Resume;
+
+/**
+ * Array based storage for Resumes
+ */
+public interface Storage {
+
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
+}
