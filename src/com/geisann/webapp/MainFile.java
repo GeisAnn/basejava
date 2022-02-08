df8a960ed8684203797debe1ffbac0b9635c6610
GeisAnn <pushyduke@yandex.ru>
2022-02-08 10:52:04 +0300

    Do indents in findFiles

--- src/com/geisann/webapp/MainFile.java
+++ src/com/geisann/webapp/MainFile.java
@@ -3,22 +3,22 @@ package com.geisann.webapp;
 import java.io.File;
 
 public class MainFile {
-    public static void findFiles(File dir) {
+    public static void findFiles(File dir, String indent) {
         File[] list = dir.listFiles();
         if (list != null) {
             for (File file : list) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(indent + "File: " + file.getName());
                 } else if (dir.isDirectory()) {
-                    System.out.println("Directory: " + dir.getName());
-                    findFiles(file);
+                    System.out.println(indent + "Directory: " + file.getName());
+                    findFiles(file, "   ");
                 }
             }
         }
     }
 
     public static void main(String[] args) {
-        File dir = new File("C://Users//a//Documents//GitHub//basejava");
-        findFiles(dir);
+        File dir = new File("./src/com/geisann/webapp");
+        findFiles(dir, "");
     }
 }
